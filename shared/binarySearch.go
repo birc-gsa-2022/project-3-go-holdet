bd8d116190986543deb0888191823b9837593b33
Simon <au649801@uni.au.dk>
2022-10-30 11:37:34 +0100

    BS

--- shared/binarySearch.go
+++ shared/binarySearch.go
@@ -2,67 +2,63 @@ package shared
 
 import (
 	"fmt"
-	"math"
+	"strings"
 )
 
 /*Performs binary search on
 First we identify a match. When we the match, we branch out and find upper and lower bound.*/
 func BinarySearch(genome string, read string, suffixArray []int) (int, int) {
-	// default integer divison rounds down
-	low := 0
-	high := len(suffixArray) - 1
 
-	//edgecase where read is empty. Return empty interval (genome[0:0[)
-	if read == "" {
+	//add sentinel if not present
+	var sb strings.Builder
+	if genome[len(genome)-1] != '$' {
+		sb.WriteString(genome)
+		sb.WriteByte('$')
+		genome = sb.String()
+	}
+
+	//edgecase where read is empty or longer than genome. Return empty interval (genome[0:0[)
+	if read == "" || len(read) > len(genome) {
 		return 0, 0
 	}
-
-	for low < high-1 {
-		mid := high - (high-low)/2
-		saIndex := suffixArray[mid]
-		//check if match (only on strings at least as long as read)
-		if saIndex+len(read) < len(genome) {
-			if genome[saIndex:saIndex+len(read)] == read {
-				//a match has been found now search for upper and lower bound.
-				upperBound := upperBound(mid, high, suffixArray, genome, read)
-				lowerBound := lowerBound(mid, low, suffixArray, genome, read)
-				return lowerBound, upperBound
-			}
-		}
-		fmt.Println(genome[saIndex:])
-		if genome[saIndex:] > read {
-			high = mid
-		} else {
-			low = mid + 1
-		}
+	fmt.Println("we good")
+	upperBound := upperBound(suffixArray, genome, read)
+	lowerBound := lowerBound(suffixArray, genome, read)
+	if upperBound != lowerBound {
+		return lowerBound, upperBound
 	}
-	//case where no match is found. Interval genome[0:0[ is empty.
 	return 0, 0
+
 }
 
-func upperBound(match int, high int, suffixArray []int, genome string, read string) int {
-	low := match
+func upperBound(suffixArray []int, genome string, read string) int {
+	low := 0
+	high := len(genome) - 1
 
 	//we break when we are left with two values. The last element that matches, and the first element that does not.
 	for low < high-1 {
-		//round up
-		mid := low + int(math.Ceil(float64(high-low)/2.0))
+		fmt.Println("up")
+		fmt.Println(low, high)
+		mid := low + (high-low)/2
 		saIndex := suffixArray[mid]
+		fmt.Println(genome[saIndex:], read)
 
-		if saIndex+len(read) < len(genome) {
-			if genome[saIndex:saIndex+len(read)] > read {
-				//mid is too high
-				high = mid
-			} else {
-				//we have some match (might not be greatest match)
+		if suffixArray[mid]+len(read) < len(genome) {
+			if genome[suffixArray[mid]:suffixArray[mid]+len(read)] == read {
 				low = mid
+				continue
 			}
-			//case where suffix is shorter than read
+		}
+		if genome[saIndex:] < read {
+			//mid is too high
+			low = mid
 		} else {
+			//mid is too low
 			high = mid
 		}
 
 	}
+
 	//We return the idx of first element that is not included, since we define intervals as '[i,j['. Result is either high or high+1.
 	if suffixArray[high]+len(read) < len(genome) {
 		if genome[suffixArray[high]:suffixArray[high]+len(read)] == read {
@@ -75,24 +71,34 @@ func upperBound(match int, high int, suffixArray []int, genome string, read stri
 		return high
 	}
 }
-func lowerBound(match int, low int, suffixArray []int, genome string, read string) int {
-	high := match
+func lowerBound(suffixArray []int, genome string, read string) int {
+	low := 0
+	high := len(genome) - 1
 
 	//we break when we are left with two values. The last element before match, and the first match.
 	for low < high-1 {
-
+		fmt.Println("down")
+		fmt.Println(low, high)
 		mid := low + (high-low)/2
 		saIndex := suffixArray[mid]
+		fmt.Println(genome[saIndex:], read)
 
+		if suffixArray[mid]+len(read) < len(genome) {
+			if genome[suffixArray[mid]:suffixArray[mid]+len(read)] == read {
+				high = mid
+				continue
+			}
+		}
 		if genome[saIndex:] < read {
 			//mid is too low
 			low = mid
 		} else {
-			//we have some match (might not be smallest match)
+			//mid is too high
 			high = mid
 		}
 
 	}
+
 	//check if value low is the match, and return if it is. Otherwise match is low+1. (high)
 	if suffixArray[low]+len(read) < len(genome) {
 		if genome[suffixArray[low]:suffixArray[low]+len(read)] == read {
@@ -103,5 +109,4 @@ func lowerBound(match int, low int, suffixArray []int, genome string, read strin
 	} else {
 		return high
 	}
-
 }
